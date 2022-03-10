aca9a3569864e59e3783710ad6b11a05ccad1099
KyoheiTaniguchi <kyohei.taniguchi@asbloom.me>
2022-03-10 17:14:43 -0300

    #2_movie6

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,82 @@
-
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:test1/next_page.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 void main() => runApp(MyApp());
 
-
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(kyohei: 'nann',),
+      home: MyHomePage(
+        kyohei: 'nann',
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key,required this.kyohei}) : super(key: key);
-final String kyohei;
-
+  const MyHomePage({Key? key, required this.kyohei}) : super(key: key);
+  final String kyohei;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
-      
       _counter++;
     });
   }
 
-
-String text = '次へ';
+  String text = '次へ';
 
   @override
   Widget build(BuildContext context) {
-
+    var center = Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: double.infinity,
+            child: Text(
+              'KBOYさん',
+              textAlign: TextAlign.end,
+              style: TextStyle(
+                fontSize: 40,
+              ),
+            ),
+          ),
+          DefaultTextStyle(
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.purple,
+              ),
+              child: Column(
+                children: [
+                  Text('ジーコさん'),
+                  Text('ジーコさん'),
+                  Text('ジーコさん'),
+                ],
+              ))
+        ],
+      ),
+    );
     return Scaffold(
       appBar: AppBar(
-        
         title: Text('KBOYのFlutter大学！！'),
-        
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text(text),
-        onPressed: () async { 
-       final result = await Navigator.push(
-           context,
-           MaterialPageRoute(builder: (context) => NextPage('じーこさん'),
-           ),
-         );
-         
-
-             setState(() {
-      
-    text = result;
-    });
-        },
-    ),
-    ),
-        
-      
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-       // This trailing comma makes auto-formatting nicer for build methods.
-    ),
-  );
-  }}
+      body: center,
+    );
+  }
+}
