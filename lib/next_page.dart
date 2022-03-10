ed3556fde81df43b29323d17d46b81baf67c0439
daichi.yasuda <daichi.yasuda@asbloom.me>
2022-03-10 16:50:19 -0300

    feat: setup analysis $5

--- lib/next_page.dart
+++ lib/next_page.dart
@@ -1,37 +1,33 @@
-
 import 'package:flutter/material.dart';
 
 class NextPage extends StatelessWidget {
- NextPage(this.name);
-final String name;
-  
+  const NextPage(this.name, {Key? key}) : super(key: key);
+  final String name;
+
   @override
   Widget build(BuildContext context) {
-  
-return Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: Text('次の画面'),
+        title: const Text('次の画面'),
       ),
       body: Container(
         height: double.infinity,
-        color: Colors. red ,
+        color: Colors.red,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children:[
+          children: [
             Text(name),
-            
-        Center(
-           child: ElevatedButton(
-          child: const Text('戻る'),
-        onPressed: () {
-          
-          Navigator.pop(context, 'ジーコさんかっこいい');
-          }
-    ),
-    ),
-        ],
+            Center(
+              child: ElevatedButton(
+                child: const Text('戻る'),
+                onPressed: () {
+                  Navigator.pop(context, 'ジーコさんかっこいい');
+                },
+              ),
+            ),
+          ],
         ),
-    ),
-  );
+      ),
+    );
   }
-  }
+}
