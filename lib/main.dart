487d5b8fe044207ba875e01e8170155d57533f22
KyoheiTaniguchi <kyohei.taniguchi@asbloom.me>
2022-03-10 18:02:34 -0300

    #2 movie7 test

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:test1/next_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -14,13 +12,16 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const MyHomePage(
+        kyohei: 'nann',
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({Key? key, required this.kyohei}) : super(key: key);
+  final String kyohei;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -31,38 +32,43 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final center = Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(
+            width: double.infinity,
+            height: 100,
+            child: Text(
+              'KBOYさん',
+              textAlign: TextAlign.end,
+              style: TextStyle(
+                fontSize: 40,
+              ),
+            ),
+          ),
+          DefaultTextStyle(
+            style: const TextStyle(
+              fontSize: 20,
+              color: Colors.purple,
+            ),
+            child: Column(
+              children: const [
+                Text('ジーコさん'),
+                Text('ジーコさん'),
+                Text('ジーコさん'),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
     return Scaffold(
       appBar: AppBar(
         title: const Text('KBOYのFlutter大学！！'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text(text),
-          onPressed: () async {
-            final result = await Navigator.push<String>(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const NextPage('じーこさん'),
-              ),
-            );
-            if (result != null) {
-              setState(() {
-                text = result;
-              });
-            }
-          },
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if (kDebugMode) {
-            print('ボタンを押しました');
-          }
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-        // This trailing comma makes auto-formatting nicer for build methods.
-      ),
+      body: center,
     );
   }
 }
