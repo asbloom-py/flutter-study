3655d2e38e52f363e5794b981f38df6a56507cc4
KyoheiTaniguchi <kyohei.taniguchi@asbloom.me>
2022-03-15 22:34:36 -0300

    #2　動画９のはじめ

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(
-        kyohei: 'nann',
+        kyohei: 'テスト',
       ),
     );
   }
