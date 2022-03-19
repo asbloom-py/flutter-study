40bde1d0fb0114ce3c5247b09a2ae0c4b4b7448f
TakuroNagasawa <takuro.nagasawa@asbloom.me>
2022-03-19 19:58:10 -0300

    #8_MisPedidosScreen

--- lib/main copy.dart
+++ lib/main copy.dart
@@ -12,29 +12,63 @@ class MyApp extends StatelessWidget {
           colorScheme: const ColorScheme.light(primary: Colors.orange),
         ),
         home: Scaffold(
-            appBar: AppBar(
-              centerTitle: true,
-              title: const Text(
-                'DestName',
-              ),
-              titleTextStyle: const TextStyle(color: Colors.black),
-              leading: const Icon(
-                Icons.clear,
-                color: Colors.black,
-              ),
+          appBar: AppBar(
+            centerTitle: true,
+            title: const Text(
+              'DestName',
             ),
-            body: Card(
-              elevation: 4.0,
-              margin: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Image.asset('assets/neko1_600x400.jpg'),
-                  _titleArea(),
-                  _buttonArea(),
-                  _descriptionArea()
-                ],
-              ),
-            )));
+            titleTextStyle: const TextStyle(color: Colors.black),
+            leading: const Icon(
+              Icons.clear,
+              color: Colors.black,
+            ),
+          ),
+          body: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                    padding: EdgeInsets.only(
+                      top: 15,
+                    ),
+                    color: Colors.transparent,
+                    height: 100,
+                    width: 170,
+                    child: Column(
+                      children: const [
+                        Text('recogida',
+                            style: TextStyle(
+                                fontSize: 25,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold)),
+                        Text('19/03 9:00~12:00',
+                            style: TextStyle(height: 2.5, fontSize: 15))
+                      ],
+                    )),
+                VerticalDivider(
+                  color: Colors.grey,
+                  thickness: 3,
+                  endIndent: 640,
+                ),
+                Container(
+                    padding: EdgeInsets.only(
+                      top: 15,
+                    ),
+                    color: Colors.transparent,
+                    height: 100,
+                    width: 170,
+                    child: Column(
+                      children: const [
+                        Text('Entrega',
+                            style: TextStyle(
+                                fontSize: 25,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold)),
+                        Text('19/03 13:00~15:00',
+                            style: TextStyle(height: 2.5, fontSize: 15))
+                      ],
+                    )),
+              ]),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -9,46 +9,68 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData.from(
-          colorScheme: const ColorScheme.light(primary: Colors.orange),
+          colorScheme: const ColorScheme.light(primary: Colors.white),
         ),
         home: Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            title: const Text(
-              'DestName',
+            appBar: AppBar(
+              centerTitle: true,
+              title: const Text(
+                'DestName',
+              ),
+              titleTextStyle: const TextStyle(color: Colors.black),
+              leading: const Icon(
+                Icons.clear,
+                color: Colors.black,
+              ),
             ),
-            titleTextStyle: const TextStyle(color: Colors.black),
-            leading: const Icon(
-              Icons.clear,
-              color: Colors.black,
-            ),
-          ),
-          body: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
+            body: Column(
               children: [
-                Container(
-                    padding: EdgeInsets.only(
-                      top: 15,
-                    ),
-                    color: Colors.transparent,
-                    height: 100,
-                    width: 170,
-                    child: Column(
-                      children: const [
-                        Text('recogida',
-                            style: TextStyle(
-                                fontSize: 25,
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold)),
-                        Text('19/03 9:00~12:00',
-                            style: TextStyle(height: 2.5, fontSize: 15))
-                      ],
-                    )),
-                VerticalDivider(
-                  color: Colors.grey,
-                  thickness: 3,
-                  endIndent: 640,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                        padding: EdgeInsets.only(
+                          top: 15,
+                        ),
+                        color: Colors.transparent,
+                        height: 100,
+                        width: 170,
+                        child: Column(
+                          children: const [
+                            Text('recogida',
+                                style: TextStyle(
+                                    fontSize: 25,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold)),
+                            Text('19/03 9:00~12:00',
+                                style: TextStyle(height: 2.5, fontSize: 15))
+                          ],
+                        )),
+                    // VerticalDivider(
+                    //   color: Colors.grey,
+                    //   thickness: 3,
+                    //   endIndent: 640,
+                    // ),
+                    Container(
+                        padding: EdgeInsets.only(
+                          top: 15,
+                        ),
+                        color: Colors.transparent,
+                        height: 100,
+                        width: 170,
+                        child: Column(
+                          children: const [
+                            Text('Entrega',
+                                style: TextStyle(
+                                    fontSize: 25,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold)),
+                            Text('19/03 13:00~15:00',
+                                style: TextStyle(height: 2.5, fontSize: 15))
+                          ],
+                        )),
+                  ],
                 ),
                 Container(
                     padding: EdgeInsets.only(
@@ -68,7 +90,19 @@ class MyApp extends StatelessWidget {
                             style: TextStyle(height: 2.5, fontSize: 15))
                       ],
                     )),
-              ]),
-        ));
+              ],
+            )));
+  }
+}
+
+class Easyoperation extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(Icons.whatsapp),
+        Text('Envia mensaje a asigar'),
+        Icon(Icons.arrow_back)
+      ],
+    );
   }
 }
