76181ca160c122de42196e8b9cab350e0737c349
TakuroNagasawa <takuro.nagasawa@asbloom.me>
2022-03-20 04:34:02 -0300

    #8_MisPedidosScreen_Politicadecancelacion

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class MyApp extends StatelessWidget {
                         width: 170,
                         child: Column(
                           children: const [
-                            Text('recogida',
+                            Text('Recogida',
                                 style: TextStyle(
                                     fontSize: 25,
                                     color: Colors.black,
@@ -47,11 +47,11 @@ class MyApp extends StatelessWidget {
                                 style: TextStyle(height: 2.5, fontSize: 15))
                           ],
                         )),
-                    // VerticalDivider(
-                    //   color: Colors.grey,
-                    //   thickness: 3,
-                    //   endIndent: 640,
-                    // ),
+                    VerticalDivider(
+                      color: Colors.black,
+                      thickness: 3,
+                      endIndent: 64,
+                    ),
                     Container(
                         padding: EdgeInsets.only(
                           top: 15,
@@ -72,37 +72,87 @@ class MyApp extends StatelessWidget {
                         )),
                   ],
                 ),
-                Container(
-                    padding: EdgeInsets.only(
-                      top: 15,
+                Column(
+                  children: [
+                    ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Color.fromARGB(255, 223, 223, 223),
+                        child: Icon(
+                          Icons.mode_edit,
+                          color: Colors.black,
+                        ),
+                      ),
+                      title: Text('Modifica fecha y hora'),
+                      trailing: Icon(
+                        Icons.arrow_right,
+                        color: Colors.black,
+                      ),
                     ),
-                    color: Colors.transparent,
-                    height: 100,
-                    width: 170,
-                    child: Column(
-                      children: const [
-                        Text('Entrega',
-                            style: TextStyle(
-                                fontSize: 25,
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold)),
-                        Text('19/03 13:00~15:00',
-                            style: TextStyle(height: 2.5, fontSize: 15))
-                      ],
-                    )),
+                    Divider(),
+                    ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Color.fromARGB(255, 223, 223, 223),
+                        child: Icon(
+                          Icons.whatsapp,
+                          color: Colors.black,
+                        ),
+                      ),
+                      title: Text('Envia mensaje a asigar'),
+                      trailing: Icon(Icons.arrow_right),
+                    ),
+                    Divider(),
+                    ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Color.fromARGB(255, 223, 223, 223),
+                        child: Icon(
+                          Icons.whatsapp,
+                          color: Colors.black,
+                        ),
+                      ),
+                      title: Text('Envia mensaje al destinatario'),
+                      trailing: Icon(Icons.arrow_right),
+                    ),
+                    Divider(),
+                    ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Color.fromARGB(255, 223, 223, 223),
+                        child: Icon(
+                          Icons.block,
+                          color: Colors.black,
+                        ),
+                      ),
+                      title: Text('Cancela este pedido'),
+                      trailing: Icon(Icons.arrow_right),
+                    ),
+                    Divider(),
+                    Container(
+                      margin: EdgeInsets.only(top: 20, left: 5, right: 5),
+                      width: double.infinity,
+                      height: 100,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Color.fromARGB(255, 245, 227, 227),
+                          border: Border.all(color: Colors.black)),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Icon(Icons.notification_important_sharp),
+                              Text(
+                                ' Politica de cancelacion',
+                                style:
+                                    TextStyle(color: Colors.red, fontSize: 23),
+                              ),
+                            ],
+                          ),
+                          Text('キャンセルには、キャンセル料金がかかります。詳細はキャンセルポリシーをご覧ください。')
+                        ],
+                      ),
+                    )
+                  ],
+                )
               ],
             )));
-  }
-}
-
-class Easyoperation extends StatelessWidget {
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Icon(Icons.whatsapp),
-        Text('Envia mensaje a asigar'),
-        Icon(Icons.arrow_back)
-      ],
-    );
   }
 }
