8b7481b5717d0c23894fc9aa049d88afc98b0fed
KyoheiTaniguchi <kyohei.taniguchi@asbloom.me>
2022-03-19 22:10:00 -0300

    youtubeの動画のリストを作るとこ

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const App());
+void main() {
+  runApp(
+    App(
+      items: List<String>.generate(10000, (i) => 'Item $i'),
+    ),
+  );
+}
 
 class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+  final List<String> items;
 
+  // ignore: sort_constructors_first
+  const App({Key? key, required this.items}) : super(key: key);
+
+  @override
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -51,38 +61,54 @@ class App extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Row(
-              children: [
-                Image.network(
-                  'https://s3-ap-northeast-1.amazonaws.com/qiita-organization-image/562149f7ec4ab60dc93eb10dc4877d9453946b73/original.jpg?1599561074',
-                  width: 100,
-                ),
-                Column(
-                  children: [
-                    const Text('KBOYのFlutter大学'),
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: 44,
-                          child: TextButton(
-                            style: TextButton.styleFrom(
-                              textStyle: const TextStyle(fontSize: 20),
-                            ),
-                            onPressed: () {
-                              //to do
-                            },
-                            child: const Icon(
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 60,
+                    height: 60,
+                    child: Image.network(
+                      'https://s3-ap-northeast-1.amazonaws.com/qiita-organization-image/562149f7ec4ab60dc93eb10dc4877d9453946b73/original.jpg?1599561074',
+                      width: 100,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Column(
+                    children: [
+                      const Text('KBOYのFlutter大学'),
+                      TextButton(
+                        child: Row(
+                          // ignore: prefer_const_literals_to_create_immutables
+                          children: [
+                            const Icon(
                               Icons.video_call,
-                              color: Colors.black,
+                              color: Colors.red,
                             ),
-                          ),
+                            const Text(
+                              '登録する',
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ],
                         ),
-                        const Text('登録する'),
-                      ],
-                    )
-                  ],
-                )
-              ],
+                        onPressed: () {
+                          //todo
+                        },
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(items[index]),
+                );
+              },
             )
           ],
         ),
