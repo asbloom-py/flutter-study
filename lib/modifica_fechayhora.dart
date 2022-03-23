8c79d86870146343bd0fb4e7cd790ac9629b4d63
TakuroNagasawa <takuro.nagasawa@asbloom.me>
2022-03-23 14:52:56 -0300

    #8_modifica_fechayhora

---
+++ lib/modifica_fechayhora.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Fechayhora extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hora'),
+      ),
+      body: Container(
+        height: double.infinity,
+        color: Colors.red,
+      ),
+    );
+  }
+}
